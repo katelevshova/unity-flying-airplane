a0d4a4abf3f7ec9c397c70ba0394263173df89b6
Kateryna Levshova <>
2020-08-20 17:57:22 -0700

    Removed not used variables

--- FlyingAirplane/Assets/FlyingAirplane/Scripts/Airplane.cs
+++ FlyingAirplane/Assets/FlyingAirplane/Scripts/Airplane.cs
@@ -6,7 +6,6 @@ public class Airplane : MonoBehaviour
 {
     public float rotationSpeed = 90f;
     public float speed = 10f;
-    private bool isNormalMode = true;
     public GameObject target;
 
     // Start is called before the first frame update
@@ -20,31 +19,27 @@ public class Airplane : MonoBehaviour
     public void FollowTarget(Vector3 targetPosition)
     {
         Debug.Log("->FollowTarget "+ targetPosition);
-        isNormalMode = false;
 
         Vector3 directionToFace = (target.transform.position - transform.position).normalized;
         Debug.DrawRay(transform.position, directionToFace, Color.green);
         transform.rotation = Quaternion.LookRotation(directionToFace);
 
 
-        transform.position += transform.forward * speed * Time.deltaTime ;
     }
 
     public void Update()
     {
-        if (isNormalMode)
-        {
-            transform.position += transform.forward * speed * Time.deltaTime;
-        }
+        transform.position += transform.forward * speed * Time.deltaTime;
     }
 
     private void UpdateFlyingMode()
     {
-        isNormalMode = true;
+        Debug.Log("EndDrag");
     }
 
     private void OnDisable()
     {
         InputController.OnDragTarget -= FollowTarget;
+        InputController.OnEndDragTarget -= UpdateFlyingMode;
     }
 }
