2776eaf0dd48492077204c3f47a255bc18c6216e
Kateryna Levshova <>
2020-08-31 23:38:22 -0700

    Added new scripts

---
+++ FlyingAirplane/Assets/FlyingAirplane/Scripts/LookAtMouse.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+public class LookAtMouse : MonoBehaviour
+{
+	void Update()
+	{
+		//If the a MouseLocation script is in the scene and its mouse location is valid...
+		if (MouseLocation.Instance != null && MouseLocation.Instance.IsValid)
+		{
+			//Point the Z axis of this game object at it
+			transform.LookAt(MouseLocation.Instance.MousePosition);
+		}
+	}
+}
