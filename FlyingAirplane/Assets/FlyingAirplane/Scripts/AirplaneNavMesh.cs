c8e1dc9a196241bc8d7f023d14a3e14e0023c996
Kateryna Levshova <>
2020-09-01 00:04:46 -0700

    Added NavMesh area and a NavMeshAgent capsule for testing

---
+++ FlyingAirplane/Assets/FlyingAirplane/Scripts/AirplaneNavMesh.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AirplaneNavMesh : MonoBehaviour
+{
+	public float Duration;                          //How long the ally stays spawned
+
+	[SerializeField] NavMeshAgent navMeshAgent;     //A reference to the ally's navmesh agent
+
+	void Reset()
+	{
+		//Get a reference to the navmesh agent
+		navMeshAgent = GetComponent<NavMeshAgent>();
+	}
+
+	public void Update()
+	{
+		//Tell the navmesh agent to move to the designated point
+		navMeshAgent.SetDestination(GameController.Instance.target.transform.position);
+	}
+}
