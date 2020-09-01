0113a0ab893f9be3d9a5a22d6536edbfeef0df91
Kateryna Levshova <>
2020-08-31 23:40:04 -0700

    Changed fly towards, now it is in Update

--- FlyingAirplane/Assets/FlyingAirplane/Scripts/Airplane.cs
+++ FlyingAirplane/Assets/FlyingAirplane/Scripts/Airplane.cs
@@ -9,7 +9,9 @@ public class Airplane : MonoBehaviour
     private float rotationSpeed = 140f;
     [SerializeField]
     private float speed = 1f;
-    private bool _isNotDraggingMode = true;
+    private bool _isStart = true;
+    private Target target;
+    // IS a trigger
 
     private void Awake()
     {
@@ -21,30 +23,37 @@ public class Airplane : MonoBehaviour
         Debug.Log("[Airplane] Start");
         GameController.OnDragTarget += FlyTowards;
         GameController.OnEndDragTarget += FreeFlying; 
+        target = GameController.Instance.target;
     }
-
+    
     private void FreeFlying()
     {
-        _isNotDraggingMode = true;
+        
     }
-
     private void FlyTowards(Target target)
     {
-        _isNotDraggingMode = false;
-
-        Vector3 directionToFace = (target.transform.position - transform.position).normalized;
-        var dotProd = Vector3.Dot(transform.right, directionToFace);
-
-        transform.rotation *= Quaternion.AngleAxis(dotProd * rotationSpeed * Time.deltaTime, Vector3.up);
-
-        transform.position += transform.forward * speed * Time.deltaTime * Mathf.Abs(1 - dotProd);
-    }
+        _isStart = false;
+    } 
 
     public void Update()
     {
-        if (_isNotDraggingMode)
+        if (_isStart)
         {
             transform.position += transform.forward * speed * Time.deltaTime;
+        }
+        else
+        {
+            Vector3 directionToFace = (target.transform.position - transform.position).normalized;
+            var dotProd = Vector3.Dot(transform.right, directionToFace);
+
+            transform.rotation *= Quaternion.AngleAxis(dotProd * rotationSpeed * Time.deltaTime, Vector3.up);
+
+            transform.position += transform.forward * speed * Time.deltaTime * Mathf.Abs(1 - dotProd);
+
+            /*
+            float step = speed * Time.deltaTime ; // calculate distance to move
+            transform.position = Vector3.MoveTowards(transform.position, GameController.Instance.target.transform.position, step);
+            */
         }
     }
 
